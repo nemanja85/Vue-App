971d47828335a4f66f5dcb9f0d41deb6f796d74e
Nemanja <nemanja.mitic@msgconsulting.net>
2020-10-26 13:59:00 +0100

    add firebase to the project

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import store from './store'
 
 import './assets/formulate.css'
 import VueFormulate from '@braid/vue-formulate'
+import firebase from "firebase"
 
-import VueFormGenerator from 'vue-form-generator'
-import 'vue-form-generator/dist/vfg.css'
+Vue.use(VueFormulate)
 
-Vue.use(VueFormulate, VueFormGenerator)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKX7g8KuSpbc_rklZ51bxMB_p0z9I6S-Q",
+  authDomain: "my-project-4acfb.firebaseapp.com",
+  databaseURL: "https://my-project-4acfb.firebaseio.com",
+  projectId: "my-project-4acfb",
+  storageBucket: "my-project-4acfb.appspot.com",
+  messagingSenderId: "329852580096",
+  appId: "1:329852580096:web:8fde6453ebb490e97dc3e5",
+  measurementId: "G-XMWJSNV8EX"
+};
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
