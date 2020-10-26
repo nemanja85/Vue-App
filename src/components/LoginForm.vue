eb72e579bd1b047c52bd688a262a57a8526e8128
Nemanja <nemanja.mitic@msgconsulting.net>
2020-10-26 16:26:04 +0100

    create method to Sign in with Email and Password

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,43 +1,48 @@
 <template>
-    <div class="container-fluid">
-        <FormulateForm class="inputs" @submit="login">
-            <h2 class="form-title">Log in</h2>
-            <FormulateInput
-            type="email"
-            name="email"
-            v-model="email"
-            label="Email address"
-            validation="required|email"
-            />
-            <FormulateInput
-            type="password"
-            name="password"
-            v-model="password"
-            label="Password"
-            validation="required" 
-            />
-            <FormulateInput label="Login" type="submit" />
-        </FormulateForm>
-    </div>
-  
+  <div class="container-fluid">
+    <FormulateForm class="inputs" @submit="login">
+      <h2 class="form-title">Log in</h2>
+      <FormulateInput
+        type="email"
+        name="email"
+        v-model="email"
+        label="Email address"
+        validation="required|email"
+      />
+      <FormulateInput
+        type="password"
+        name="password"
+        v-model="password"
+        label="Password"
+        validation="required"
+      />
+      <FormulateInput label="Login" type="submit" />
+    </FormulateForm>
+  </div>
 </template>
 
 <script>
+import firebase from "firebase"
 export default {
   data: () => ({
-      email:"",
-      password:""
+    email: "",
+    password: "",
   }),
   methods: {
     login() {
-      console.log(this.email + this.password)
-    }
-  }
-}
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((user) =>{
+            console.log(user)
+        })
+        .catch(function (error) {
+          console.log( "Unable to login: " + error.message)
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-
-
 </style>
